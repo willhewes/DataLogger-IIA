b5bfca49c04a077cd9c5b1a7e8e51dfa2e60e267
Will Hewes <william.hewes03@gmail.com>
2025-05-27 15:20:08 +0100

    Init for tmp.ino - temp monitoring module

---
+++ Arduino/tmp/tmp.ino
@@ -0,0 +1,39 @@
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-tmp36-temperature-sensor
+ */
+
+#define ADC_VREF 3.3 // in volt
+#define ADC_RESOLUTION 1024.0
+#define PIN_TMP36 A0
+
+void setup()
+{
+    Serial.begin(9600);
+    analogReference(EXTERNAL); // set the voltage reference from VREF pin
+}
+
+void loop()
+{
+    // get the ADC value from the TMP36 temperature sensor
+    int adcVal = analogRead(PIN_TMP36);
+    // convert the ADC value to voltage
+    float voltage = adcVal * (ADC_VREF / ADC_RESOLUTION);
+    // convert the voltage to the temperature in Celsius
+    float tempC = (voltage - 0.5) * 100;
+    // convert the Celsius to Fahrenheit
+    float tempF = tempC * 9 / 5 + 32;
+
+    // print the temperature in the Serial Monitor:
+    Serial.print("TMP36 Temperature: ");
+    Serial.print(tempC); // print the temperature in Celsius
+    Serial.print("°C");
+    Serial.print("  ~  "); // separator between Celsius and Fahrenheit
+    Serial.print(tempF);   // print the temperature in Fahrenheit
+    Serial.println("°F");
+
+    delay(1000);
+}
